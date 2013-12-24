5230b997b20eb7d70a9c5afdd4811eb5ce5aea43
wangjohn <wangjohn@mit.edu>
2013-12-24 14:42:29 -0500

    Adding multiprocessing queues.

--- kinder/main_generator.py
+++ kinder/main_generator.py
@@ -1,23 +1,33 @@
 import random
 import unittest
 import test_job
+import multiprocessing
 
 class MainGenerator:
-    def __init__(self, test_jobs, param_generators, options = None):
+    def __init__(self, test_jobs, param_generators, num_prcoesses = 5):
         self.test_jobs = MarkovProcess(test_jobs)
         self.param_generators = {key: MarkovProcess(gens) for key, gens in param_generators.iteritems()}
-        self.unit_test_runner = UnitTestRunner()
-        self.options = options
+        self.unit_test_runners = []
+        self.num_processes = 5
 
         self._enforce_invariants(self.test_jobs, self.param_generators)
 
-    def run(self):
-        pass
+    def run(self, num_jobs = 1000):
+        queue = multiprocessing.Queue()
+        for i in xrange(num_jobs):
+            queue.put(self.step())
+
+        for i in xrange(self.num_processes):
+            runner = UnitTestRunner()
+            self.unit_test_runners.append(runner)
+            QueueRunner(queue, runner).start()
+        results = [runner.result for runner in self.unit_test_runners]
+        return results
 
     def step(self):
         test = self.test_jobs.generate()
         params = self.param_generators[test].generate()
-        self.unit_test_runner.run_tests(test, params)
+        return (test, params)
 
     def _enforce_invariants(self, test_jobs, param_generators):
         if len(param_generators) != len(test_jobs.prob_hash):
@@ -27,6 +37,17 @@ class MainGenerator:
                 raise "Must subclass TestJob class for all tests"
             if key not in param_generators:
                 raise "test_jobs and param_generators inputs must match"
+
+class QueueRunner(multiprocessing.Process):
+    def __init__(self, queue, unit_test_runner):
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        self.unit_test_runner = unit_test_runner
+
+    def run(self):
+        for item in iter(self.queue.get, "STOP"):
+            test, params = item[0], item[1]
+            self.unit_test_runner.run_tests(test, params)
 
 class UnitTestRunner:
     def __init__(self, result = None):
