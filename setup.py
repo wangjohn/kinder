dd6811a6bf83216d6b32ede8c56b0a4f073138c3
wangjohn <wangjohn@mit.edu>
2013-12-25 00:53:48 -0500

    Changing setup

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-        name='Kinder',
-        version='0.1.0',
-        author='John J. Wang',
-        author_email='john@zinc.io',
-        packages=['kinder'],
-        scripts=[],
-        url='',
-        license='LICENSE',
-        description='Probabilistic testing framework.',
-        long_description=open('README.md').read(),
-        install_requires=[
-            "unittest >= 1.0.0"
-            ],
-        )
+    name='Kinder',
+    version='0.1.3',
+    author='John J. Wang',
+    author_email='john@zinc.io',
+    packages=['kinder'],
+    scripts=[],
+    url='',
+    license=read('LICENSE'),
+    description='Probabilistic testing framework.',
+    long_description="A probabilistic testing framework designed for testing API endpoints.",
+    install_requires=[
+        "unittest >= 1.0.0"
+        ],
+    )
